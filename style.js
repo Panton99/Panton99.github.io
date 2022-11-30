8e4ada0829f8be44735edad25b703fb336b04487
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2022-11-30 16:02:42 -0500

    Update style.js

--- style.js
+++ style.js
@@ -17,7 +17,7 @@ const valSubmit = (event) =>{
     const form = event.target;
     const data = new FormData(form);
 
-    fetch("/" , {
+    fetch("/contact.html" , {
         method: "POST",
         headers: {"Content-type": "application/x-www-form-urlencoded"},
         body: new URLSearchParams(data).toString(),
@@ -26,41 +26,3 @@ const valSubmit = (event) =>{
 };
 document.querySelector("form").addEventListener("submit", valSubmit);
 
-
-
-
-// function validateName(val, status) {
-//     var regex = /^[a-zA-Z]*$/;
-//     if(val.value.length > 0) {
-//         return true;
-//     }
-//     if (regex.test(val.value)) {
-//         return true;
-//     }
-// }
-
-// function validiation(val) {
-//     val.preventDefault();
-//     var data = new FormData(val.target);
-//     valFName = validateName(document.getElementById('firstName'), valFName);
-//     valLName = validateName(document.getElementById('lastName'), valLName);
-//     var msg = documnte.getElementById("submitMsg");
-
-//     if (!valFName || !valLName) {
-//         alert("Type your name.");
-//     }
-
-//     fetch(val.target.action, {
-//         method: form.method,
-//         body: data,
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     }).then(res => {
-//         msg.innerHTML = "Thank you for the submission!";
-//         form.reset();
-//     }).catch(error => {
-//         msg.innerHTML = "There was a problem submitting your form. Try again."
-//     });
-// }
-// form.addEventListener("submit", validiation)
