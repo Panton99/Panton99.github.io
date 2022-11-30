b4b40793e78e930fbcdc8fb8093e6670497710a5
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2022-11-30 16:41:48 -0500

    Update style.js

--- style.js
+++ style.js
@@ -7,24 +7,24 @@ function onClick() {
     element.classList.toggle("dark-mode");
 }
 
-// var form = document.getElementById("form");
-// var valFName = false;
-// var valLName = false;
-// var valEmail = false;
+var form = document.getElementById("form");
+var valFName = false;
+var valLName = false;
+var valEmail = false;
 
-// const valSubmit = (event) =>{
-//     event.preventDefault();
-//     const form = event.target;
-//     const data = new FormData(form);
+const valSubmit = (event) =>{
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(form);
 
-//     fetch("/contact.html" , {
-//         method: "POST",
-//         headers: {'Content-type': "application/x-www-form-urlencoded"},
-//         body: new URLSearchParams(data).toString(),
-//     }).then(() => console.log("Successfully submitted")).catch((error) => alert("Somethings wrong!"));
+    fetch("/contact" , {
+        method: "POST",
+        headers: {'Content-type': "application/x-www-form-urlencoded"},
+        body: new URLSearchParams(data).toString(),
+    }).then(() => console.log("Successfully submitted")).catch((error) => alert("Somethings wrong!"));
 
-// };
-// document.querySelector("form").addEventListener("submit", valSubmit);
+};
+document.querySelector("form").addEventListener("submit", valSubmit);
 
 
 
