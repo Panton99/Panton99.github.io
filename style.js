0cd2d550fa5a264e8be4c62e083e02e038d6ec83
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2022-11-30 14:50:51 -0500

    Add files via upload

--- style.js
+++ style.js
@@ -7,4 +7,60 @@ function onClick() {
     element.classList.toggle("dark-mode");
 }
 
+var form = document.getElementById("form");
+var valFName = false;
+var valLName = false;
+var valEmail = false;
 
+const valSubmit = (event) =>{
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(form);
+
+    fetch("/" , {
+        method: "POST",
+        headers: {"Content-type": "application/x-www-form-urlendcoded"},
+        body: new URLSearchParams(data).toString(),
+    }).then(() => console.log("Successfully submitted")).catch((error) => alert("Somethings wrong!"));
+
+};
+document.querySelector("form").addEventListener("submit", valSubmit);
+
+
+
+
+// function validateName(val, status) {
+//     var regex = /^[a-zA-Z]*$/;
+//     if(val.value.length > 0) {
+//         return true;
+//     }
+//     if (regex.test(val.value)) {
+//         return true;
+//     }
+// }
+
+// function validiation(val) {
+//     val.preventDefault();
+//     var data = new FormData(val.target);
+//     valFName = validateName(document.getElementById('firstName'), valFName);
+//     valLName = validateName(document.getElementById('lastName'), valLName);
+//     var msg = documnte.getElementById("submitMsg");
+
+//     if (!valFName || !valLName) {
+//         alert("Type your name.");
+//     }
+
+//     fetch(val.target.action, {
+//         method: form.method,
+//         body: data,
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }).then(res => {
+//         msg.innerHTML = "Thank you for the submission!";
+//         form.reset();
+//     }).catch(error => {
+//         msg.innerHTML = "There was a problem submitting your form. Try again."
+//     });
+// }
+// form.addEventListener("submit", validiation)
